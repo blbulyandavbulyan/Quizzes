e86113161dd9677b63747ebca57efec8304753f3
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-04-10 10:34:28 +0400

    added QuizFragment

---
+++ app/src/main/java/com/blbulyandavbulan/quizzes/QuizFragment.kt
@@ -0,0 +1,99 @@
+package com.blbulyandavbulan.quizzes
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.blbulyandavbulan.quizzes.databinding.FragmentQuizBinding
+import com.blbulyandavbulan.quizzes.quiz.Quiz
+import com.google.android.material.radiobutton.MaterialRadioButton
+
+private const val QUIZ_PARAM = "QUIZ"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [QuizFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class QuizFragment : Fragment() {
+    private var quiz: Quiz? = null
+    private var _binding: FragmentQuizBinding? = null
+    private val binding: FragmentQuizBinding
+        get() = _binding!!
+    private var answersRadioGroups: List<List<RadioGroup>>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            //я знаю что этот метод depracated, однако для использования альтернативы нужно повысить уровень minSdk
+            // чего я пока делать не хочу
+            quiz = it.getSerializable(QUIZ_PARAM) as Quiz
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQuizBinding.inflate(inflater, container, false)
+        binding.quizzesLinearLayout
+        val answersRadioGroups = mutableListOf<RadioGroup>()
+        quiz?.questions?.forEach { question ->
+            val context = requireContext()
+            val questionTextView = TextView(context)
+            questionTextView.text = question.question
+            questionTextView.id = View.generateViewId()
+            questionTextView.typeface = Typeface.defaultFromStyle(Typeface.BOLD)
+            questionTextView.gravity = Gravity.CENTER
+            questionTextView.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            questionTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 15f)
+            binding.quizzesLinearLayout.addView(questionTextView)
+            val radioGroup = RadioGroup(context)
+            question.answers.forEach { answer ->
+                val answerRadioButton = MaterialRadioButton(context)
+                answerRadioButton.text = answer
+                answerRadioButton.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                radioGroup.addView(answerRadioButton)
+                radioGroup.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                )
+                radioGroup.parent?.let {//что-то не понятное взятое с форумов, без него вываливается в исключение
+                    // что у view уже есть parent
+                    (it as ViewGroup).removeView(radioGroup)
+                }
+                binding.quizzesLinearLayout.addView(radioGroup)
+            }
+            answersRadioGroups.add(radioGroup)
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(quiz: Quiz) =
+            QuizFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(QUIZ_PARAM, quiz)
+                }
+            }
+    }
+}
