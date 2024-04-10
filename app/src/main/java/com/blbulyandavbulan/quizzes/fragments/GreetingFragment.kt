d6b994b3aa3a7597e32ed338ba650f29ef269d25
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-04-10 12:12:59 +0400

    removed onCreate overriding from GreetingFragment

--- app/src/main/java/com/blbulyandavbulan/quizzes/fragments/GreetingFragment.kt
+++ app/src/main/java/com/blbulyandavbulan/quizzes/fragments/GreetingFragment.kt
@@ -19,10 +19,6 @@ class GreetingFragment : Fragment() {
         _binding = null
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
