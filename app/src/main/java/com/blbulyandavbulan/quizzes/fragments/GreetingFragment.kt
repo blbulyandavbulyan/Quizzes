e553188e4bbd6d6903ce74eb0cbf7c919ab601b8
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-04-21 19:05:23 +0400

    added showing date time picker by pressing button in GreetingFragment

--- app/src/main/java/com/blbulyandavbulan/quizzes/fragments/GreetingFragment.kt
+++ app/src/main/java/com/blbulyandavbulan/quizzes/fragments/GreetingFragment.kt
@@ -1,16 +1,21 @@
 package com.blbulyandavbulan.quizzes.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.blbulyandavbulan.quizzes.R
 import com.blbulyandavbulan.quizzes.databinding.FragmentGreetingBinding
 import com.blbulyandavbulan.quizzes.quiz.QuizStorage
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.snackbar.Snackbar
+import java.time.Instant
+import java.util.TimeZone
 
 class GreetingFragment : Fragment() {
+    private var selectedDateOfBirth: Long? = null
     private var _binding: FragmentGreetingBinding? = null
     private val binding: FragmentGreetingBinding
         get() = _binding!!
@@ -33,6 +38,26 @@ class GreetingFragment : Fragment() {
             findNavController().navigate(R.id.action_GreetingFragment_To_QuizFragment, Bundle().apply {
                 putSerializable("QUIZ", QuizStorage.getQuiz())
             });
+        }
+
+        binding.selectDateOfBirthButton.setOnClickListener { _ ->
+            val dataPicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText(R.string.select_date_of_birth)
+                .apply {
+                    selectedDateOfBirth?.let {
+                        setSelection(selectedDateOfBirth)
+                    }
+                }.build()
+            dataPicker.addOnPositiveButtonClickListener {
+                this.view?.let { view ->
+                    Snackbar.make(view, Instant.ofEpochMilli(it).atZone(TimeZone.getDefault().toZoneId()).toLocalDate().toString(), Snackbar.LENGTH_SHORT).show()
+                    binding.continueButton.isEnabled = true
+                }
+            }
+            fragmentManager?.let {
+                dataPicker.show(it, "DataPicker")
+            }
+
         }
     }
 }
